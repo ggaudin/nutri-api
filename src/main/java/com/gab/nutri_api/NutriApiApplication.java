9080dd2758f149e9c815e5304fa7f9d69e7585d5
ggaudin <gabrielle.gaudin@gmail.com>
2026-06-02 17:11:02 +0200

    initialisation api spring boot

---
+++ src/main/java/com/gab/nutri_api/NutriApiApplication.java
@@ -0,0 +1,13 @@
+package com.gab.nutri_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriApiApplication.class, args);
+	}
+
+}
